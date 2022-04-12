85beb3a547837947c184c7bf5ccd70b5dbf6af93
hamada mayia <hamadamayia@gmail.com>
2022-04-12 22:50:04 +0300

    first Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MapRoot from './components/mapRoot';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+      <MapRoot />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/providers.js
@@ -0,0 +1,7 @@
+const providers = {
+    maptiler: {
+        url: "https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=rwySVDj9dnHoyXFbdx8z",
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }
+}
+export default providers
